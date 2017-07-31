f754eeccc2ceae3c12753e9747050fb516948b48
Igor <pasieczny.igor@gmail.com>
2017-07-31 17:30:33 +0200

    add TinyMCEAnnotationBodyEditor template

--- js/src/annotations/tinymce-annotation-editor.js
+++ js/src/annotations/tinymce-annotation-editor.js
@@ -139,12 +139,10 @@
       });
     },
 
-    editorTemplate: $.Handlebars.compile([
-      '<textarea class="text-editor" placeholder="{{t "comments"}}…">{{#if content}}{{content}}{{/if}}</textarea>',
-      '<select id="tags-editor-{{windowId}}" class="tags-editor" multiple="true">{{#each tags}}',
-      '<option value="{{this}}" {{#ifContains ../selectedTags this }}selected="selected"{{/ifContains}}>{{this}}</option>',
-      '{{/each}}</select>'
-    ].join(''))
+    editorTemplate: function (data) {
+      console.log("tinymce-annotation-editor LOG");
+      return $.Handlebars.getTemplate(this.state.getStateProperty('template'), 'annotations/tinymce-annotation-editor')(data);
+    }
   };
 
   // Handlebars helper: Contains
